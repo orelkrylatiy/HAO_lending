b1cff89315c99ce9ac098bc2ec02fcbeefba0bb4
Агафонов Максим <m.s.agafonov@tbank.ru>
2026-08-10 11:57:52 +0500

    Add signup modal deep links

--- app/components/SignupClientBoundary.tsx
+++ app/components/SignupClientBoundary.tsx
@@ -6,6 +6,9 @@ import LeadModal from "./LeadModal";
 import FloatingCTA from "./FloatingCTA";
 import type { Lang } from "@/app/lib/dictionaries";
 
+const SIGNUP_MODAL_PARAM = "modal";
+const SIGNUP_MODAL_VALUE = "signup";
+
 interface Props {
   lang?: Lang;
 }
@@ -14,19 +17,46 @@ export default function SignupClientBoundary({ lang = "ru" }: Props) {
   const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
-    const openModal = () => setModalOpen(true);
+    const isSignupModalRequested = () =>
+      new URLSearchParams(window.location.search).get(SIGNUP_MODAL_PARAM) === SIGNUP_MODAL_VALUE;
+
+    const openModal = () => {
+      const url = new URL(window.location.href);
+
+      if (!isSignupModalRequested()) {
+        url.searchParams.set(SIGNUP_MODAL_PARAM, SIGNUP_MODAL_VALUE);
+        window.history.pushState(null, "", url);
+      }
+
+      setModalOpen(true);
+    };
+
+    const syncModalWithUrl = () => setModalOpen(isSignupModalRequested());
 
     window.addEventListener("hao:open-signup", openModal);
+    window.addEventListener("popstate", syncModalWithUrl);
+    syncModalWithUrl();
+
     return () => {
       window.removeEventListener("hao:open-signup", openModal);
+      window.removeEventListener("popstate", syncModalWithUrl);
     };
   }, []);
 
+  const closeModal = () => {
+    const url = new URL(window.location.href);
+    url.searchParams.delete(SIGNUP_MODAL_PARAM);
+    window.history.replaceState(null, "", url);
+    setModalOpen(false);
+  };
+
+  const openModal = () => window.dispatchEvent(new CustomEvent("hao:open-signup"));
+
   return (
     <>
-      <Header lang={lang} onSignup={() => setModalOpen(true)} />
-      <FloatingCTA lang={lang} onClick={() => setModalOpen(true)} />
-      <LeadModal lang={lang} isOpen={modalOpen} onClose={() => setModalOpen(false)} />
+      <Header lang={lang} onSignup={openModal} />
+      <FloatingCTA lang={lang} onClick={openModal} />
+      <LeadModal lang={lang} isOpen={modalOpen} onClose={closeModal} />
     </>
   );
 }
